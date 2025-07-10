afde86b1b21dcb2858fab86d73af93840a7910ac
Vivek r <ramdurgvivek630@gmail.com>
2025-07-10 16:20:32 +0530

    Add evaluation module with hooks and backend routes

---
+++ client/src/pages/Admin/Evaluation/EvaluationInterview.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const EvaluationInterview = () => {
+  return (
+    <div>
+      <li>🧪 Evaluation (Marks Entry & Interviews)
+      <ul>
+        <li>Marks Entry</li>
+        <li>Schedule interviews</li>
+        <li>Assign interviewers and manage results</li>
+        <li>Track Evaluated and Unevaluated students (These are whose marks and interviews are not conducted yet).</li>
+      </ul>
+      </li>
+    </div>
+  )
+}
+
+export default EvaluationInterview;
